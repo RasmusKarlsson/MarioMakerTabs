56ab7effdf3ed3462546397da581e59972996d6d
Rasmus Karlsson <raska293@student.liu.se>
2015-10-13 18:13:27 +0200

    fix

--- main.js
+++ main.js
@@ -88,7 +88,6 @@ window.onload = function()
           for(var i = 0;i<notes.length;i++)
           {
               var height = 26*32-notes[i].note*32;
-              if(i == 0) console.log(height);
               if(height < 0)
                 writeWarning("Notes got clipped, too HIGH pitched for Mario Maker. Try again with an offset of "+(TAB_OFFSET+height/32));
 
